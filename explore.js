33f7b4608906d19db486b58f792898154af28f6b
Sumayea Rahman <shorbonam104@gmail.com>
2023-03-30 20:59:56 +0600

    36-1

--- explore.js
+++ explore.js
@@ -1,3 +1,9 @@
 // 01
 // create object ,using object literals
 const player = {};
+player.name = 'small Nirob';
+
+player.specialty = 'batsman';
+player.bat = function(){
+    console.log('swing your bat')
+}
